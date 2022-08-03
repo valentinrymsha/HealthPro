15498c3232f312ac0d11e5b95d639fc7ab508740
valentinrymsha <valentinrymsha@gmail.com>
2022-08-03 18:44:07 +0300

    Initial Commit

---
+++ HealthPro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HealthPro
+//
+//  Created by User on 8/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
